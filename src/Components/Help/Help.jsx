e4a63d216856ef8e913190de7c3c1bf743d40f9f
AdnanAraf <arafadnan10@gmail.com>
2023-12-08 11:49:04 +0600

    Help

--- src/Components/Help/Help.jsx
+++ src/Components/Help/Help.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Help = () => {
   return (
@@ -24,7 +25,62 @@ const Help = () => {
       </div>
 
       <div>
-        <div></div>
+        <div className="grid grid-cols-3 text-center mx-[200px] mt-[100px]   gap-[50px]">
+          <Link
+            to="/"
+            className="bg-[#efefec] h-[50px] p-[13px] w-[350px] rounded-full text-[16px] font-bodyFont font-semibold hover:bg-[#dbdad2] hover:border-[#2B381F] border-2"
+          >
+            New to AllTrails
+          </Link>
+          <Link
+            to="/"
+            className="bg-[#efefec] h-[50px] p-[13px] w-[350px] rounded-full text-[16px] font-bodyFont font-semibold hover:bg-[#dbdad2] hover:border-[#2B381F] border-2"
+          >
+            Account & Privacy
+          </Link>
+          <Link
+            to="/"
+            className="bg-[#efefec] h-[50px] p-[13px] w-[350px] rounded-full text-[16px] font-bodyFont font-semibold hover:bg-[#dbdad2] hover:border-[#2B381F] border-2"
+          >
+            Subscription & Billing
+          </Link>
+          <Link
+            to="/"
+            className="bg-[#efefec] h-[50px] p-[13px] w-[350px] rounded-full text-[16px] font-bodyFont font-semibold hover:bg-[#dbdad2] hover:border-[#2B381F] border-2"
+          >
+            AllTrails+
+          </Link>
+          <Link
+            to="/"
+            className="bg-[#efefec] h-[50px] p-[13px] w-[350px] rounded-full text-[16px] font-bodyFont font-semibold hover:bg-[#dbdad2] hover:border-[#2B381F] border-2"
+          >
+            Using AllTrails
+          </Link>
+          <Link
+            to="/"
+            className="bg-[#efefec] h-[50px] p-[13px] w-[350px] rounded-full text-[16px] font-bodyFont font-semibold hover:bg-[#dbdad2] hover:border-[#2B381F] border-2"
+          >
+            AllTrails Community
+          </Link>
+          <Link
+            to="/"
+            className="bg-[#efefec] h-[50px] p-[13px] w-[350px] rounded-full text-[16px] font-bodyFont font-semibold hover:bg-[#dbdad2] hover:border-[#2B381F] border-2"
+          >
+            Troubleshooting
+          </Link>
+          <Link
+            to="/"
+            className="bg-[#efefec] h-[50px] p-[13px] w-[350px] rounded-full text-[16px] font-bodyFont font-semibold hover:bg-[#dbdad2] hover:border-[#2B381F] border-2"
+          >
+            Contact
+          </Link>
+          <Link
+            to="/"
+            className="bg-[#efefec] h-[50px] p-[13px] w-[350px] rounded-full text-[16px] font-bodyFont font-semibold hover:bg-[#dbdad2] hover:border-[#2B381F] border-2"
+          >
+            AllTrails Member Forum
+          </Link>
+        </div>
       </div>
     </div>
   );
